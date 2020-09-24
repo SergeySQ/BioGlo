f8bf86852d8da49c4fb6b9b9c95f1c4e16d228f7
sergey <mketsvil2@gmail.com>
2020-09-25 01:59:02 +0600

    Реализовал вызов модального окна

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,5 +1,25 @@
 'use strict';
 const togglePopUp = () => {
-    alert('hello');
+
+    const body = document.body,
+        PopUpCall = document.querySelector('.popup-call');
+
+    body.addEventListener('click', (e) => {
+        const target = e.target;
+        //открытие модального окна по кнопке "перезвоните"
+        if (target.closest('.call-btn')) {
+            e.preventDefault();
+            PopUpCall.style.cssText = "display:block;opacity:1;";
+            PopUpCall.classList.add("active-popup");
+        } else if (target.closest('.popup-close')) {
+            //закрытие модального окна по крестику
+            target.closest('.active-popup').removeAttribute('style');
+            target.closest('.active-popup').classList.remove('active-popup');
+        } else if (target.closest('.active-popup') && !target.closest('.popup-content')) {
+            //закрытие модального окна по подложке
+            target.closest('.active-popup').removeAttribute('style');
+            target.closest('.active-popup').classList.remove('active-popup');
+        }
+    });
 };
 export default togglePopUp;
